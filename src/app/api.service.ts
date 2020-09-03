77f0f4a24d3428c84110809c106fe4b01b7d822b
Zack Johanson <packerfan4life12@yahoo.com>
2020-09-03 19:20:40 -0400

    added pip

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,6 +7,6 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  apiUrl:string = ''
+  apiUrl:string = 'https://api.airtable.com/v0/appFo187B73tuYhyg/Master%20List?api_key=key1EsUq5RZwXesvD'
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
