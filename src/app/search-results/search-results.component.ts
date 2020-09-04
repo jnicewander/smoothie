2131ffcd5844c221b1d4a4b3774b72b9b9e7c1d9
J.Nicewander <jnicewander@live.com>
2020-09-04 15:11:09 -0400

    Working get request and rudementary information displayed.

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { MasterlistResponse } from '../interfaces/masterlist-response';
+import { MasterlistResponse, Records } from '../interfaces/masterlist-response';
 
 @Component({
   selector: 'app-search-results',
@@ -10,16 +10,15 @@ import { MasterlistResponse } from '../interfaces/masterlist-response';
 export class SearchResultsComponent implements OnInit {
 
   constructor(private api: ApiService) { }
-  data: MasterlistResponse[]; 
+  startUps: Records[];
 
   ngOnInit(): void {
     this.getMasterList();
   }
 
   getMasterList() {
-    this.api.getMaster().subscribe((response: MasterlistResponse[]) => {
-      this.data = response;
+    this.api.getMaster().subscribe((response: MasterlistResponse) => {
+      this.startUps = response.records;
     })
   }
-
 }
