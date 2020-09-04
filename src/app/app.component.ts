4c93951004e42e82734ede82c9a4f79eadcd4262
Zack Johanson <packerfan4life12@yahoo.com>
2020-09-03 21:49:22 -0400

    added ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService} from './api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'smoothie';
+  data = [];
+  constructor(private api: ApiService) {
+
+  }
+  ngOnInit(): void {
+    this.api.getStartups().subscribe((res: any) => {
+      this.data = res;
+    })
+  }
 }
