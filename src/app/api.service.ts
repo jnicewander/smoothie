a7cf30f004a96821951a870a3fd4fdb98a9cc683
Zack Johanson <packerfan4life12@yahoo.com>
2020-09-03 21:48:16 -0400

    refactored getStartups method

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,12 +9,9 @@ export class ApiService {
   constructor(private http: HttpClient) { }
   apiUrl:string = 'https://api.airtable.com/v0/appFo187B73tuYhyg/Master%20List?api_key=key1EsUq5RZwXesvD'
 
-  getStartups(endPoint:string, query?:string, page?: number){
-    if(page) {
-      return this.http.get(`${this.apiUrl}${endPoint}${query}${page}`)
-    } else {
-      return this.http.get(`${this.apiUrl}${endPoint}${query}`)
+    getStartups(){
+      return this.http.get(this.apiUrl)
     }
   }
 
-}
+
