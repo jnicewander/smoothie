473687feb62d8d19f7218b40040928730fbef389
J.Nicewander <jnicewander@live.com>
2020-09-04 11:09:06 -0400

    Get method created from gitignore file of variables. Will only work in local development with tempVars.ts file.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { URL } from './tempVars';
 
 @Injectable({
   providedIn: 'root',
@@ -7,14 +8,8 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  apiUrl:string = 'https://api.airtable.com/v0/appFo187B73tuYhyg/Master%20List?api_key=key1EsUq5RZwXesvD'
-
-  getStartups(endPoint:string, query?:string, page?: number){
-    if(page) {
-      return this.http.get(`${this.apiUrl}${endPoint}${query}${page}`)
-    } else {
-      return this.http.get(`${this.apiUrl}${endPoint}${query}`)
-    }
+  
+  getMaster() {
+    return this.http.get(`${URL.BASE}${URL.TABLE_REF.MASTER}?api_key=${URL.API_KEY}`);
   }
-
 }
