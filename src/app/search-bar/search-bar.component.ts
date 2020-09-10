790f62a576bfc092d6c6c507881c9fe94dc7bc4d
J.Nicewander <jnicewander@live.com>
2020-09-10 15:05:44 -0400

    Changed ApiService import location to match new folder.

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { ApiService } from './api.service.ts';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-search-bar',
