3f11408ae3cb8494e3dbc5e4b79c2048d75433b2
Patrick <pjohnson24@icloud.com>
2020-09-10 20:57:03 -0400

    finishing favorites component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
-import { FilterPipe } from './filter.pipe';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FilterPipe } from './filter.pipe';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ThemeCardComponent } from './theme-card/theme-card.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { FormsModule } from '@angular/forms';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
@@ -20,8 +19,6 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
     FilterPipe,
     NavbarComponent,
     ThemeCardComponent,
-    SearchResultsComponent,
-    FavoriteComponent,
     SearchBarComponent
 
   ],
