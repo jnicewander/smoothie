b2914dc8b1acbc65092915824ec3731be004d038
J.Nicewander <jnicewander@live.com>
2020-09-11 17:04:25 -0400

    Reconfigured service to do less to avoid the use of observables until the team has more time to learn how to use them.

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,25 +1,11 @@
-import { Injectable } from '@angular/core';
-import { ApiService } from './api.service';
-import { MasterListResponse, Records } from '../interfaces/master-list-response';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  searchResults: Records[];
-  constructor(private api: ApiService) { }
+  searchQuery = new EventEmitter();
 
-  // onSearch will be called onClick in the search-bar component
-  // the user text will be passed in as an argument(query).
-  onSearch(query: string) {
-    this.api.getMasterList().subscribe((response: MasterListResponse) => {
-      // filter "response.records" here to include any objects containing the "query"
-      // set the return value equal to "searchResults"
-    })
-  }
-  // create another function to return the "searchResults" variable as an observable
-  // subscribe to the newly created function inside the search results 
-  // component that hasn't been created yet.
+  constructor() { }
+
 }
-
-// Alternatively, ask Chris about using query params "OR({<field name>} = '<search text>', etc...)"
