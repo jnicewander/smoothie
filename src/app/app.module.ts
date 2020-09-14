49382ce3ff967243ea81ec53695b9f5afcb630cb
Patrick <pjohnson24@icloud.com>
2020-09-14 12:51:42 -0400

    simplified routing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,8 +10,7 @@ import { FilterPipe } from './filter.pipe';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ThemeCardComponent } from './theme-card/theme-card.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
+
 
 
 
@@ -22,9 +21,8 @@ import { SearchResultsComponent } from './search-results/search-results.componen
     NavbarComponent,
     ThemeCardComponent,
     SearchBarComponent,
-    FavoritesComponent,
-    SearchResultsComponent
-
+    routingComponent,
+   
   ],
   imports: [
     BrowserModule,
