690fd6f0b2945ea52c0882e79c2130d310d424c5
J.Nicewander <jnicewander@live.com>
2020-09-14 14:30:04 -0400

    Added event listener until we can discuss observables together.

--- src/app/services/details.service.ts
+++ src/app/services/details.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailsService {
+  getDetails = new EventEmitter();
 
   constructor() { }
 }
