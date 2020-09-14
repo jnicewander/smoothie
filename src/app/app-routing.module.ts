62cf13d39a3c8ede7fdf8fd7315fce0ce29ca8aa
Patrick <pjohnson24@icloud.com>
2020-09-14 10:31:09 -0400

    working on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FavoritesService } from '../app/services/favorites.service'
+import { SearchResultsComponent} from '../app/search-results/search-results.component'
 import { FavoritesComponent } from './favorites/favorites.component';
 
+
 const routes: Routes = [
-  { path: "favorites", component: FavoritesComponent},
+  {path: "", component: SearchResultsComponent},
+  { path: "favorites", component: FavoritesComponent}
 ];
 
 @NgModule({
