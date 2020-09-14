d0f8993c4fc9233076863caa9763e2d5905599e6
J.Nicewander <jnicewander@live.com>
2020-09-14 11:13:21 -0400

    Generated details component from Angular cli.

---
+++ src/app/startup-details/startup-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startup-details',
+  templateUrl: './startup-details.component.html',
+  styleUrls: ['./startup-details.component.css']
+})
+export class StartupDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
