e061a38cf4fdc54c237a234c06d676873e906ff4
J.Nicewander <jnicewander@live.com>
2020-09-15 12:08:09 -0400

    Added query strings to search other tables.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,20 +6,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
   PROXY_ROOT: string = 'https://gc-airtable-proxy.herokuapp.com/';
+  FILTER: string = '?filterByFormula=';
 
 
   constructor(private http: HttpClient) { }
   
-  getMasterList(route?: string, id?: string) {
+  getMasterList(route?: string, startup?: string) {
     let urlString: string = this.PROXY_ROOT;
-    let queryByID: string = `?filterByFormula = SEARCH(RECORD_ID()%2C+%22${id}%22)`
+    
     if (route) {
-     urlString += route;
+      urlString += route;
     } else {
       urlString += 'master-list';
     }
-    if (id) {
-      urlString += queryByID;
+    if (startup) {
+      let startupURI = startup.replace(' ', '+');
+      urlString += this.FILTER;
+      
+      if (route.toLowerCase() === 'feedback') {        
+        urlString += 'Startup%3D%22';
+      } else if (route.toLowerCase() === 'projects') {      
+        urlString += '%7BStartup+Engaged%7D%3D%22';
+      } else {        
+        urlString += '%7BCompany+Name%7D%3D%22';
+      }
+      urlString += startupURI + '%22';
+
     }
 
     return this.http.get(urlString);
