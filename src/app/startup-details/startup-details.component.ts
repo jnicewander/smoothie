c18973419944bf288f3b8709885c568b7d4484b0
J.Nicewander <jnicewander@live.com>
2020-09-15 14:30:06 -0400

    Created a method to get data from remaining tables. Created a method to merge all data into one object.

--- src/app/startup-details/startup-details.component.ts
+++ src/app/startup-details/startup-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DetailsService } from '../services/details.service';
+import { ApiService } from '../services/api.service';
 import { MasterListRecords } from '../interfaces/master-list-response';
+import { FeedbackRecords, FeedbackResponse } from '../interfaces/feedback-response';
+import { ProjectsRecords, ProjectsResponse } from '../interfaces/projects-response';
 
 @Component({
   selector: 'app-startup-details',
@@ -9,13 +12,41 @@ import { MasterListRecords } from '../interfaces/master-list-response';
 })
 export class StartupDetailsComponent implements OnInit {
   startup: MasterListRecords;
-  constructor(private details: DetailsService) { }
+  feedback: FeedbackRecords[];
+  projects: ProjectsRecords[];
+
+  constructor(private details: DetailsService, private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.details.getDetails.subscribe(startupObj => {
-      this.startup = startupObj;
-      console.log(this.startup);
+    this.details.getDetails.subscribe(response => {
+      this.getFeedbackDetails('Feedback', response.fields["Company Name"]);
+      this.getProjects('Projects', response.fields["Company Name"]);
+      this.startup = response;
+    })
+  }
+  
+  getFeedbackDetails(route: string, startup: string): void {
+    this.apiService.getMasterList(route, startup).subscribe((response: FeedbackResponse) => {
+      this.feedback = response.records;
+    })
+  }
+  
+  getProjects(route: string, startup: string): void {
+    this.apiService.getMasterList(route, startup).subscribe((response: ProjectsResponse) => {
+      this.projects = response.records;
+      this.mergeDetails();
     })
   }
 
+  mergeDetails(): void {
+    if(this.feedback) {
+      this.feedback.forEach((record) => {
+        this.startup[record.fields["Your Organization"].toLowerCase() + ' feedback'] = record;
+    })
+    if(this.projects) {
+        this.startup['projects'] = this.projects;
+    }
+    console.log(this.startup);
+    }
+  }
 }
