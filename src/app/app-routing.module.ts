f91b54411cb703595c37fc6da5c31a1acd3fc14c
Patrick <pjohnson24@icloud.com>
2020-09-15 21:53:21 -0400

    added routing for two other theme cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchResultsComponent} from '../app/search-results/search-results.component'
 import { FavoritesComponent } from './favorites/favorites.component';
+import { ThemeCardComponent } from './theme-card/theme-card.component';
+import { ThemeCard2Component } from './theme-card2/theme-card2.component';
+import { ThemeCard3Component } from './theme-card3/theme-card3.component';
 
 
 const routes: Routes = [
   {path: "", component: SearchResultsComponent},
-  { path: "favorites", component: FavoritesComponent}
+  { path: "favorites", component: FavoritesComponent},
+  { path: "theGoodLife", component: ThemeCardComponent},
+  { path: "healthBeyondTheHospital", component: ThemeCard2Component},
+  { path: "robustFuture", component: ThemeCard3Component}
 ];
 
 @NgModule({
@@ -14,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponent = [SearchResultsComponent, FavoritesComponent]
+export const routingComponent = [SearchResultsComponent, FavoritesComponent,ThemeCardComponent,ThemeCard2Component,ThemeCard3Component]
