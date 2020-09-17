e213ae7420c216570f4a206633a992d5a2769de6
J.Nicewander <jnicewander@live.com>
2020-09-17 10:51:05 -0400

    Split oninit method into two methods to remove repetition.

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../services/search.service';
 import { ApiService } from '../services/api.service';
 import { MasterListResponse, MasterListRecords } from '../interfaces/master-list-response';
@@ -13,49 +13,24 @@ import { FavoritesService } from '../services/favorites.service'
 export class SearchResultsComponent implements OnInit {
   startups: MasterListRecords[];
   offsetValues: string[] = [];
-  // filteredStartups: MasterListRecords[];
   constructor(private search: SearchService, private api: ApiService, private favoritesService: FavoritesService, private detailsService: DetailsService) {}
 
   ngOnInit(): void {
-    // this.getMasterList();
     this.search.searchQuery.subscribe((query) => {
-      // this.filterByQuery(query);
-      this.api.getTableData(query).subscribe((response: MasterListResponse) => {
-            console.log(response.offset + '  <-- The offset');
-            this.offsetValues.push(response.offset);
-            this.startups = response.records;
-          })
+      this.getData(query);
+    })
+  }
+
+  getData(query, offset?: string) {
+    this.api.getTableData(query, offset).subscribe((response: MasterListResponse) => {
+      this.offsetValues.push(response.offset);
+      this.startups = response.records;
     })
   }
 
   next() {
-    this.api.getTableData(null, this.offsetValues[this.offsetValues.length -1]).subscribe((response: MasterListResponse) => {
-      this.offsetValues.push(response.offset);
-      this.startups = response.records
-    })
+    this.getData(null, this.offsetValues[this.offsetValues.length - 1]);
   }
-
-  previous() {
-    
-  }
-
-  // getMasterList() {
-  //   this.api.getTableData().subscribe((response: MasterListResponse) => {
-  //     this.startups = response.records;
-  //   })
-  // }
-
-  // filterByQuery(query: string) {
-  //   if (query) {
-  //     this.filteredStartups = this.startups.filter((object) => {
-  //       return Object.keys(object.fields)
-  //         .some(key => object.fields[key].toLowerCase()
-  //           .includes(query.toLowerCase()));
-  //     });
-  //   } else {
-  //     this.filteredStartups = this.startups;
-  //   }
-  // }
 
   showDetails(startup) {
     this.detailsService.getDetails.emit(startup);
