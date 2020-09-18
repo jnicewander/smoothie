cc4a3fcfc596c1980025827a9a3816b0a290f2bb
J.Nicewander <jnicewander@live.com>
2020-09-18 14:54:43 -0400

    Moved methods out of component and into service.

--- src/app/services/details.service.ts
+++ src/app/services/details.service.ts
@@ -1,10 +1,24 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { ApiService } from './api.service';
+import { FeedbackResponse } from '../interfaces/feedback-response';
+import { ProjectsResponse } from '../interfaces/projects-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailsService {
   getDetails = new EventEmitter();
+  setFeedbackDetails = new EventEmitter();
+  setProjectDetails = new EventEmitter();
 
-  constructor() { }
+  constructor(private apiService: ApiService) {
+    this.getDetails.subscribe((response) => {
+      this.apiService.getDetails('Feedback', response.fields["Company Name"]).subscribe((responseF: FeedbackResponse) => {
+        this.setFeedbackDetails.emit(responseF);
+      }) 
+      this.apiService.getDetails('Projects', response.fields["Company Name"]).subscribe((responseP: ProjectsResponse) => {
+        this.setProjectDetails.emit(responseP);
+      })  
+    })
+  }
 }
