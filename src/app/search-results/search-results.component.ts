cb73fc573ae9bf0f543aa985391417109ee85a58
jnicewander <jnicewander@live.com>
2020-09-19 17:10:11 -0400

    Added a function to find the query matches from response. The API does not provide this functionality.

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -13,6 +13,7 @@ import { FavoritesService } from '../services/favorites.service'
 export class SearchResultsComponent implements OnInit {
   startups: MasterListRecords[];
   offsetValues: string[] = [];
+  matchedProperties = [];
   constructor(private search: SearchService, private api: ApiService, private favoritesService: FavoritesService, private detailsService: DetailsService) {}
 
   ngOnInit(): void {
@@ -25,7 +26,27 @@ export class SearchResultsComponent implements OnInit {
     this.api.getTableData(query, offset).subscribe((response: MasterListResponse) => {
       this.offsetValues.push(response.offset);
       this.startups = response.records;
+      console.log(this.startups);
+      this.appendSearchMatches(query);
     })
+  }
+
+  appendSearchMatches(query) {
+    if(query) {
+      this.matchedProperties = [];
+      let matches = [];
+      this.startups.forEach(obj => {
+        for (let [key, value] of Object.entries(obj.fields)) {
+          if (value.toLowerCase().includes(query.toLowerCase())) {
+            matches.push(`${key}`);
+          }
+        }
+        let merged = matches.join(', ')
+        this.matchedProperties.push(merged);
+        matches = [];
+      })
+      console.log(this.matchedProperties);
+    }
   }
 
   next() {
