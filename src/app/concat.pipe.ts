a8ad8c5f23283563060c1c052cf2b7aac9bebc50
jnicewander <jnicewander@live.com>
2020-09-20 11:11:52 -0400

    Added method into pipe to streamline concatination of matched properties.

--- src/app/concat.pipe.ts
+++ src/app/concat.pipe.ts
@@ -1,16 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { MasterListRecords } from './interfaces/master-list-response';
 
 @Pipe({
   name: 'concat'
 })
 export class ConcatPipe implements PipeTransform {
-
-  transform(startupArray, matchedArray): void {
-    if (startupArray) {
-      startupArray.forEach((obj, i) => {
-        obj['matches'] = matchedArray[i];
-      })
+  matchedProperties: string[];
+  transform(startup: MasterListRecords, query: string): MasterListRecords {
+    if (startup) {
+      this.matchedProperties = [];
+      let matches = [];
+      for (let [key, value] of Object.entries(startup.fields)) {
+        if (value.toLowerCase().includes(query)) {
+          matches.push(key);
+        }
+      }
+      let merged = matches.join(', ')
+      this.matchedProperties.push(merged);
+      matches = [];
+      startup['matches'] = this.matchedProperties;
     }
-    return startupArray;
+    console.log(startup);
+    return startup;
   }
 }
