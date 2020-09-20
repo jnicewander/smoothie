c361ac988304c088b509fb747bfd02fbb94ec384
jnicewander <jnicewander@live.com>
2020-09-20 11:11:18 -0400

    Added a new container component for main view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, routingComponent } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { NavbarComponent } from './navbar/navbar.component';
-import { ThemeCardComponent } from './theme-card/theme-card.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { ResultsListComponent } from './results-list/results-list.component';
+import { ThemeCardComponent } from './theme-card/theme-card.component';
 import { StartupDetailsComponent } from './startup-details/startup-details.component';
 import { ConcatPipe } from './concat.pipe';
+import { MainViewComponent } from './main-view/main-view.component';
 
 @NgModule({
   declarations: [
@@ -19,8 +23,11 @@ import { ConcatPipe } from './concat.pipe';
     ThemeCardComponent,
     SearchBarComponent,
     StartupDetailsComponent,
-    routingComponent,
-    ConcatPipe
+    SearchResultsComponent,
+    FavoritesComponent,
+    ResultsListComponent,
+    ConcatPipe,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
