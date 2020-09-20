96202e30539e022e3e69e9cc7abbda04e0b56e58
jnicewander <jnicewander@live.com>
2020-09-20 11:20:48 -0400

    Removed unused methods.

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, SimpleChange, SimpleChanges } from '@angular/core';
 import { SearchService } from '../services/search.service';
 import { ApiService } from '../services/api.service';
 import { MasterListResponse, MasterListRecords } from '../interfaces/master-list-response';
@@ -10,32 +10,17 @@ import { FavoritesService } from '../services/favorites.service'
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css'],
 })
-export class SearchResultsComponent implements OnInit {
-  startups: MasterListRecords[];
-  offsetValues: string[] = [];
-  matchedProperties: string[];
+export class SearchResultsComponent implements OnInit, OnChanges {
+  @Input() singleStartup: MasterListRecords;
+
   constructor(private search: SearchService, private api: ApiService, private favoritesService: FavoritesService, private detailsService: DetailsService) {}
 
   ngOnInit(): void {
-    this.search.searchResults.subscribe((response: MasterListResponse) => {
-      this.startups = response.records;
-      this.offsetValues.push(response.offset);
-    })
-    this.search.matchedProps.subscribe((response) => {
-      this.matchedProperties = response;
-    })
   }
 
-
-  next() {
-    this.api.getTableData(null, this.offsetValues[this.offsetValues.length - 1]).subscribe((response: MasterListResponse) => {
-      this.startups = response.records;
-      this.offsetValues.push(response.offset);
-    });
-  }
-
-  showDetails(startup) {
-    this.detailsService.getDetails.emit(startup);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['startups']) {
+    }
   }
 
   addFavorite(startup){
