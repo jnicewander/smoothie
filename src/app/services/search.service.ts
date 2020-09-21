b921509c312314a76a4b2986707c5a48270b6d3e
jnicewander <jnicewander@live.com>
2020-09-20 20:58:01 -0400

    Added event listener to reload data on new search.

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { MasterListResponse, MasterListRecords } from '../interfaces/master-list-response';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { Router } from '@angular/router';
 export class SearchService {
   searchResults: MasterListResponse;
   query: string;
+  sendIt = new EventEmitter();
 
   constructor(private apiService: ApiService, private router: Router) {}
 
@@ -16,6 +18,7 @@ export class SearchService {
     this.apiService.getTableData(data.query).subscribe((response: MasterListResponse) => {
       this.query = data.query;
       this.searchResults = response;
+      this.sendIt.emit({result: this.searchResults, query: this.query})
       this.router.navigate(['results']);
     })
   }
