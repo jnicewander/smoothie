1eddadda59811bc8a9da23a4e40e0b02fc1de2e5
jnicewander <jnicewander@live.com>
2020-09-27 16:25:23 -0400

    Created method for toggling favorites instead of just adding.

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { from } from 'rxjs';
-import { MasterListRecords, MasterListFields } from '../interfaces/master-list-response';
+import { MasterListRecords } from '../interfaces/master-list-response';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +9,21 @@ export class FavoritesService {
 
   constructor() { }
 
-  addFavorite(startup:MasterListRecords){
-    this.favoriteStartups.push(startup)
-    console.log(this.favoriteStartups)
+  toggleFavorite(startup: MasterListRecords) {
+    let index = this.checkFavorites(startup);
+    if (index !== -1) {
+      this.favoriteStartups.splice(index, 1);
+    } else {
+      this.favoriteStartups.push(startup);
+    }
   }
-  
-  removeFavorite(startup:MasterListRecords){
-    let index = this.favoriteStartups.findIndex(item=>item.id===startup.id)
-    this.favoriteStartups.splice(index,1)
+
+  checkFavorites(startup: MasterListRecords) {
+    return this.favoriteStartups.findIndex(obj => obj.id === startup.id);
+  }
+
+  removeFavorite(startup: MasterListRecords) {
+    let index = this.favoriteStartups.findIndex(item => item.id === startup.id);
+    this.favoriteStartups.splice(index, 1);
   }
 }
